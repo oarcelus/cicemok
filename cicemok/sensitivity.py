28292f429e692cd1f516bf9696e718e5c22b59b4
oarcelus <oarcelus@hotmail.com>
2024-09-17 09:39:11 +0200

    add filters for evals

--- cicemok/sensitivity.py
+++ cicemok/sensitivity.py
@@ -475,13 +475,13 @@ def get_sampling_from_experiment(
         evals = evaluate_models_pool(pool, samples_q, experiment)
 
         # INVERT MIN MAX FUNCTION FOR INCREASING VOLTAGE VALUES (CHARGE)
-        xinit = min([v[0, 0] for v in evals if v is not None])
+        xinit = min([v[0, 0] for v in evals if v is not None and v[0, 0] > exclude])
         xfin = max([v[-1, 0] for v in evals if v is not None])
 
         f = [
             (
                 interpolate.interp1d(v[:, 0], v[:, 1], assume_sorted=False, fill_value="extrapolate")
-                if v is not None and v[-1, 0]/xfin > exclude
+                if v is not None and v[0, 0] > exclude
                 else None
             )
             for v in evals
